06ed5bbf473b1e33265f4bde4b1e5fcace73c14e
Wesley Xavier <wesley.xvier@gmail.com>
2024-10-21 00:02:20 -0300

    :wrench: feat: increase knowledge about loops in F#

---
+++ loop.fsx
@@ -0,0 +1,11 @@
+// loop examplo
+
+let list = [1; 2; 3; 4; 5; 6; 7]
+for j in list do
+    printfn "%d" j
+
+for i = 1 to 100 do
+    printfn "%d - %s" i "Marry Christmass!"
+
+for i = 100 downto 1 do
+    printfn "%d reversed loop" i
--- program.fsx
+++ program.fsx
@@ -1,6 +1,14 @@
+// simple function to sum values.
 let soma x y =
     x + y
-let resultado = soma 3 5
 
-if resultado > 5 then 
+let val1 = System.Console.ReadLine()
+let val2 = System.Console.ReadLine()
+let x = int(val1)
+let y = int(val2)
+let resultado = soma x y
+
+if resultado > 18 then 
     printfn "Success!"
+else
+    printfn "Result is not greater than 18!"
